9feadf42d64dc2a8fed61fc7f9978f69c70d9258
DjStitch <37543998+DjStitch@users.noreply.github.com>
2022-01-06 15:11:51 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PharmWeb'
+copyright = '2022, EasyRx Software'
+author = 'Gerald Greyvenstein'
 
 release = '0.1'
 version = '0.1.0'
